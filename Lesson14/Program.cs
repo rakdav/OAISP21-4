54dc75475a537efd6f66e8c452c3f36bf93c29bc
Andrey <rakdav@inbox.ru>
2024-11-11 14:11:43 +0200

    char

---
+++ Lesson14/Program.cs
@@ -0,0 +1,30 @@
+﻿char ch = 'q';
+char p = '\n';
+char d = (char)999;
+Console.WriteLine(d);
+int first = (int)'А';
+int last = (int)'я';
+for (int i = first; i <=last; i++)
+{
+    Console.WriteLine($"{i}-{(char)i}");
+}
+char A = 'A';
+char z = 'z';
+for (int i = A; i <= z; i++)
+{
+    Console.WriteLine($"{i}-{(char)i}");
+}
+for(int i = (int)'0'; i <= (int)'9'; i++)
+{
+    Console.WriteLine($"{i}-{(char)i}");
+}
+if (char.IsDigit('5')) Console.WriteLine("Цифра");
+else Console.WriteLine("Не цифра");
+if (char.IsLetter('я')) Console.WriteLine("Буква");
+else Console.WriteLine("Не буква");
+if (char.IsControl('\xA')) Console.WriteLine("Управляющий");
+else Console.WriteLine("Не управляющий");
+for (int i = 0; i <= char.MaxValue; i++)
+{
+    Console.WriteLine($"{i}-{(char)i}");
+}
---
+++ lab5.1/Program.cs
@@ -0,0 +1,15 @@
+﻿char[] mas = new char[15];
+Random random = new Random();
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = (char)random.Next((int)'А',(int)'я'+1);
+    Console.Write(mas[i]+" ");
+}
+Console.WriteLine();
+for(int i = 0; i < mas.Length; i++)
+{
+    if (char.IsUpper(mas[i])) mas[i] = char.ToLower(mas[i]);
+    else if (char.IsLower(mas[i]))
+        mas[i] = char.ToUpper(mas[i]);
+    Console.Write(mas[i]+" ");
+}
