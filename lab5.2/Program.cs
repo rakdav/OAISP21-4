f1d04d801e1c921bb4f8b1ab9f14cd6ba3e3f27d
Andrey <rakdav@inbox.ru>
2024-11-15 17:32:00 +0200

    lab5.2

---
+++ lab5.2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите строку:");
+string str = Console.ReadLine();
+
+//базовый уровень
+//char s = str[2];
+//char l = str[str.Length - 1];
+//str = str.Remove(2,1);
+//str = str.Insert(2, l.ToString());
+//str = str.Remove(str.Length - 1, 1);
+//str = str.Insert(str.Length,s.ToString());
+//Console.WriteLine(str);
+
+//средний уровень
+string[] mas = str.Split("  ",' ');
+Console.WriteLine(mas.Length);
