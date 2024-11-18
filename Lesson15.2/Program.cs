db9bb92672ebfa9e24242f831b68b7809aa3c398
Andrews <rakdav@inbox.ru>
2024-11-18 10:48:35 +0200

    lesson15

---
+++ Lesson15.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+StringBuilder sb1 = new StringBuilder();
+StringBuilder sb2 = new StringBuilder("Hello");
+Console.WriteLine(sb2);
+Console.WriteLine($"Length={sb2.Length}");
+Console.WriteLine($"Capacity={sb2.Capacity}");
+StringBuilder sb3 = new StringBuilder("New String",32);
+Console.WriteLine($"Length={sb3.Length}");
+Console.WriteLine($"Capacity={sb3.Capacity}");
+//Методы
+sb1.Append("First");
+Console.WriteLine(sb1);
+sb1.Insert(5, " second");
+Console.WriteLine(sb1);
+sb1.Remove(10,2);
+Console.WriteLine(sb1);
+sb1.Replace("First","Первый");
+Console.WriteLine(sb1);
+sb1.AppendFormat("Второй");
+Console.WriteLine(sb1);
+//Когда надо использовать класс String, а когда StringBuilder?
+//Microsoft рекомендует использовать класс String в следующих случаях:
+//1. При небольшом количестве операций и изменений над строками
+//2. При выполнении фиксированного количества операций объединения. В этом случае компилятор может объединить все операции объединения в одну
+//3. Когда надо выполнять масштабные операции поиска при построении строки, например IndexOf или StartsWith. Класс StringBuilder не имеет подобных методов.
+
+//Класс StringBuilder рекомендуется использовать в следующих случаях:
+//1. При неизвестном количестве операций и изменений над строками во время выполнения программы
+//2. Когда предполагается, что приложению придется сделать множество подобных операций
---
+++ Lesson15.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+string str = "Бык тупогуб, тупогубенький бычок, у быка губа бела была тупа";
+Regex regex = new Regex(@"туп(\w*)");
+MatchCollection matches = regex.Matches(str);
+if (matches.Count > 0)
+{
+    foreach (Match match in matches) Console.WriteLine(match.Value);
+}
+else
+{
+    Console.WriteLine("Совпадений нет");
+}
+
+
+string pattern = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
+string mail = "tup@mail.ru";
+Console.WriteLine(Regex.IsMatch(mail,pattern));
+Console.WriteLine(Regex.IsMatch("Голубчиков прогульщик", pattern));
+do
+{
+    Console.Write("Введите почту:");
+    mail = Console.ReadLine();
+    if (!Regex.IsMatch(mail, pattern)) Console.WriteLine("Это не почта!");
+}
+while (!Regex.IsMatch(mail, pattern));
+
